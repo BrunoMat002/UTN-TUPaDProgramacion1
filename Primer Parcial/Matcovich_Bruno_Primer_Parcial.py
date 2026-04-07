3f43a2ae7440af69b9c1841611030a0c5c13a1d9
Brunenger02 <brunomatcovich02@gmail.com>
2026-04-07 18:55:35 -0300

    Primer Punto

--- Primer Parcial/Matcovich_Bruno_Primer_Parcial.py
+++ Primer Parcial/Matcovich_Bruno_Primer_Parcial.py
@@ -11,6 +11,32 @@
 
 print("\n--Menu--")
 
-print("\n Eliga una de las siguientes opciones:")
+print("\n 1. Cargar Herramienta ")
+
+PriOpcion = input("\n Eliga una de las siguientes opciones: ")
+
+#Verificamos si el usuario ingreso correctamente un numero
+while not PriOpcion.isdigit():
+    print("\nNo ah ingresado un numero, intente nuevamente.")
+    PriOpcion = input("\n Eliga una de las siguientes opciones: ")
 
 
+#Convertimos a integer la variable priOpcion
+opcion =int(PriOpcion)
+
+if opcion == 1:
+    print("\nIngrese la cantidad de herramientas para subir: ")
+    cantHerramientasPri = input (" ")
+    #Verificamos que el usuario ingrese un numero
+    while not cantHerramientasPri.isdigit():
+        print("\nNo ah ingresado un numero intente nuevamente: ")
+        cantHerramientasPri = input (" ")
+    #Convertimos a integer la variable cantHerramientasPri    
+    cantHerramientas =int(cantHerramientasPri)
+    for i in range(cantHerramientas):
+        nombreHerramienta =input("\nIngrese el nombre de la herramienta: ")
+        #Verificamos que haya ingresado un string
+        while not nombreHerramienta.isalpha():
+            print("\nNo ah ingresado un nombre valido, porfavor intente nuevamente: ")
+            nombreHerramienta =input("\nIngrese el nombre de la herramienta: ")
+        

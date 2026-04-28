2935e8c2b4df092340a3c08b44b4b1fd8bf3dec8
Brunenger02 <brunomatcovich02@gmail.com>
2026-04-28 19:12:16 -0300

    Ejercicio 1
    Realice el ejercicio 1

---
+++ Unidad 7/Ejercicio1.py
@@ -0,0 +1,25 @@
+#1) Dado el diccionario precios_frutas
+#precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
+#1450}
+#Añadir las siguientes frutas con sus respectivos precios:
+#● Naranja = 1200
+#● Manzana = 1500
+#● Pera = 2300 
+
+
+#Tenemos este diccionario por defecto
+precios_frutas = {"Banana" : 1200, "Anana" : 2500, "Melon" : 3000, "Uva" : 1450}
+
+
+#Para agregar elementos utilizaremos "[]"
+
+precios_frutas["Naranja"] = 1200
+precios_frutas["Manzana"] = 1500
+precios_frutas["Pera"] = 2300
+
+
+#Imprimimos el diccionario para verificar que realmente se agregaron las frutas
+
+print(precios_frutas)
+
+

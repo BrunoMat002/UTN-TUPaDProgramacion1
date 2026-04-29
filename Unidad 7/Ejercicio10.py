58b267f1968e6bb8d7194a5a570e51964afc8258
Brunenger02 <brunomatcovich02@gmail.com>
2026-04-29 15:29:30 -0300

    Ejercicio 10
    Realice el ejercicio 10

--- Unidad 7/Ejercicio10.py
+++ Unidad 7/Ejercicio10.py
@@ -2,3 +2,26 @@
 #diccionario donde:
 #• Las capitales sean las claves.
 #• Los países sean los valores. 
+
+
+#Definimos el diccionario 
+paises_capitales = {
+    "Argentina": "Buenos Aires",
+    "Chile": "Santiago",
+    "Uruguay": "Montevideo",
+    "Brasil": "Brasilia"
+}
+
+#Inicializamos un diccionario vacio para el resultado
+capitales_paises = {}
+
+#Recorremos el original obteniendo clave (pais) y valor (capital)
+for pais, capital in paises_capitales.items():
+    capitales_paises[capital] = pais
+
+#Imprimimos los resultados
+print("Diccionario Original (Pais: Capital):")
+print(paises_capitales)
+
+print("\nDiccionario Invertido (Capital: Pais):")
+print(capitales_paises)

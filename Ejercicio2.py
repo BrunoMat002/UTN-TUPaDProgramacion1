f490b6fcdbca28a0efc98f17be6502662ed34dc9
Brunenger02 <brunomatcovich02@gmail.com>
2026-05-11 17:39:23 -0300

    Ejercicio 1 y 2
    Realice los puntos 1 y 2

---
+++ Ejercicio2.py
@@ -0,0 +1,14 @@
+#2) Utilizando el código del ejercicio 1, arreglar los errores para que la ejecución del programa
+#sea correcta sin necesidad de usar excepciones.
+
+a = 10
+
+b = int(input("Introduce un número: ")) #Hacemos que el valor que introduzca el usuario sea un entero y asi poder realizar la division
+
+result = a / b 
+
+print(f"Resultado: {result}")
+
+numbers = [1, 2, 3]
+
+print(numbers[2]) #Hacemos que imprima el indice maximo que es el 2
